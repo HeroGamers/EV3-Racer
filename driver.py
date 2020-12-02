1d8caefda8330463ddce0ff854b95cba2da242d8
HeroGamers <fido2603gamer@gmail.com>
2020-12-02 12:13:06 +0100

    heck this tbh.

--- driver.py
+++ driver.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 from time import sleep
-from ev3dev2.motor import MoveTank, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_D, SpeedPercent
+from ev3dev2.motor import MoveTank, LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_D, SpeedPercent
 
 driveSpeed = SpeedPercent(100)
 turnSpeed = [SpeedPercent(30), SpeedPercent(-30)]  # Right and left
 turnDegrees = 90
 
 
-backMotors = MoveTank(left_motor_port=OUTPUT_A, right_motor_port=OUTPUT_D)  # Motor on output port A and D
+backMotors = MoveTank(left_motor_port=OUTPUT_A, right_motor_port=OUTPUT_D, motor_class=LargeMotor)  # Motor on output port A and D
+#motor1 = LargeMotor(address=OUTPUT_A)
 driveMotor = MediumMotor(address=OUTPUT_B)  # Motor on output port B
 
 
 if __name__ == '__main__':
-    backMotors.on(left_speed=driveSpeed, right_speed=driveSpeed)
+    # backMotors.on(left_speed=driveSpeed, right_speed=driveSpeed)
 
     while True:
         driveMotor.on_for_degrees(speed=turnSpeed[1], degrees=turnDegrees)
         sleep(1)
         driveMotor.on_for_degrees(speed=turnSpeed[0], degrees=turnDegrees)
+        sleep(1)
+        backMotors.on_for_degrees(left_speed=driveSpeed, right_speed=driveSpeed, degrees=360)
         sleep(1)
