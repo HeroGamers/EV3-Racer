11e3e85e289becb816f0fa54e0f79c257cbf31f8
HeroGamers <fido2603gamer@gmail.com>
2020-12-02 11:58:42 +0100

    hmmm

--- driver.py
+++ driver.py
@@ -10,10 +10,12 @@ turnDegrees = 90
 backMotors = MoveTank(left_motor_port=OUTPUT_A, right_motor_port=OUTPUT_D)  # Motor on output port A and D
 driveMotor = MediumMotor(address=OUTPUT_B)  # Motor on output port B
 
-backMotors.on(left_speed=driveSpeed, right_speed=driveSpeed)
 
-while True:
-    driveMotor.on_for_degrees(speed=turnSpeed[1], degrees=turnDegrees)
-    sleep(1)
-    driveMotor.on_for_degrees(speed=turnSpeed[0], degrees=turnDegrees)
-    sleep(1)
+if __name__ == '__main__':
+    backMotors.on(left_speed=driveSpeed, right_speed=driveSpeed)
+
+    while True:
+        driveMotor.on_for_degrees(speed=turnSpeed[1], degrees=turnDegrees)
+        sleep(1)
+        driveMotor.on_for_degrees(speed=turnSpeed[0], degrees=turnDegrees)
+        sleep(1)
