ddb416c6ddc97cbd207db583670582fc8df0b473
HeroGamers <fido2603gamer@gmail.com>
2020-12-02 12:16:19 +0100

    mmmh, monkey

--- driver.py
+++ driver.py
@@ -7,8 +7,9 @@ turnSpeed = [SpeedPercent(30), SpeedPercent(-30)]  # Right and left
 turnDegrees = 90
 
 
-backMotors = MoveTank(left_motor_port=OUTPUT_A, right_motor_port=OUTPUT_D, motor_class=LargeMotor)  # Motor on output port A and D
-#motor1 = LargeMotor(address=OUTPUT_A)
+# backMotors = MoveTank(left_motor_port=OUTPUT_A, right_motor_port=OUTPUT_D, motor_class=LargeMotor)  # Motor on output port A and D
+motor1 = LargeMotor(address=OUTPUT_A)
+motor2 = LargeMotor(address=OUTPUT_D)
 driveMotor = MediumMotor(address=OUTPUT_B)  # Motor on output port B
 
 
@@ -17,8 +18,10 @@ if __name__ == '__main__':
 
     while True:
         driveMotor.on_for_degrees(speed=turnSpeed[1], degrees=turnDegrees)
+        motor1.on_for_degrees(speed=driveSpeed, degrees=360)
         sleep(1)
         driveMotor.on_for_degrees(speed=turnSpeed[0], degrees=turnDegrees)
+        motor2.on_for_degrees(speed=driveSpeed, degrees=360)
         sleep(1)
-        backMotors.on_for_degrees(left_speed=driveSpeed, right_speed=driveSpeed, degrees=360)
-        sleep(1)
+        # backMotors.on_for_degrees(left_speed=driveSpeed, right_speed=driveSpeed, degrees=360)
+        #sleep(1)
